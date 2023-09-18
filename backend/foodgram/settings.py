78a4eefbfa71cf8aeed7a4fcc5ef34f3e9ee1377
Tatiana Busel <spbguap@yandex.com>
2023-09-18 23:09:30 +0300

    yet!

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -88,8 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
-        'PASSWORD': 'mysecretpassword',
-        # 'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432),
     }
